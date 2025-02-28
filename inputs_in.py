fd01264107cddd1fc3a2418c94817ed9e00e0afe
Amartya Varma <amartya.varma@gmail.com>
2025-02-27 20:13:57 -0800

    f

--- inputs_in.py
+++ inputs_in.py
@@ -51,7 +51,7 @@ for modified_input_ids in all_modified_input_ids:
 all_padded_tensors = torch.cat(all_padded_tensors, dim=0)
 all_attention_masks = torch.cat(all_attention_masks, dim=0)
 print("all_padded_tensors", all_padded_tensors[0].tolist())
-input_ids = all_padded_tensors[1].tolist()
+input_ids = all_padded_tensors[0].tolist()
 # attention_mask = torch.ones_like(new_input_ids).to("cuda")
 
 # input_ids = tokeniser(p, return_tensors="pt").input_ids
