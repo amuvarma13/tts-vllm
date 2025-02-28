b8689e5eec99a8f4790df88a50aa5b51f8799d60
Amartya Varma <amartya.varma@gmail.com>
2025-02-27 16:21:06 -0800

    f

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 llm = LLM(model="amuvarma/luna-tts-tags")
 
-outputs = llm.generate(prompts, sampling_params)
+print(llm.tokenizer)
 
-for output in outputs:
-    prompt = output.prompt
-    generated_text = output.outputs[0].text
-    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+# outputs = llm.generate(prompts, sampling_params)
+
+# for output in outputs:
+#     prompt = output.prompt
+#     generated_text = output.outputs[0].text
+#     print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
