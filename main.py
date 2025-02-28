a29a2bbbdbffe4139b7795df889579bfb82ea372
Amartya Varma <amartya.varma@gmail.com>
2025-02-27 18:06:08 -0800

    f

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from vllm import LLM, SamplingParams
+from transformers import AutoTokenizer
 
 prompts = [
     "Hello, my name is",
@@ -7,14 +8,17 @@ prompts = [
     "The future of AI is",
 ]
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
-
+model_name = "amuvarma/luna-tts-tags"
 llm = LLM(model="amuvarma/luna-tts-tags")
+tokeniser = AutoTokenizer.from_pretrained(model_name)
 
-print(llm.tokenizer)
+tokens = tokeniser.encode("Hello, my name is")["input_ids"]
+print(tokens)
 
-# outputs = llm.generate(prompts, sampling_params)
 
-# for output in outputs:
-#     prompt = output.prompt
-#     generated_text = output.outputs[0].text
-#     print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+outputs = llm.generate(prompt_token_ids=tokens, sampling_params=sampling_params, max_length=100)
+
+for output in outputs:
+    prompt = output.prompt
+    generated_text = output.outputs[0].text
+    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
