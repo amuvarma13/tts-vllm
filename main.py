ed5bacf15b89e4993005aa596e6532b560512564
Amartya Varma <amartya.varma@gmail.com>
2025-03-05 17:31:09 -0800

    f

--- main.py
+++ main.py
@@ -112,4 +112,4 @@ def sse():
     return Response(sse_event_stream(prompt), mimetype='text/event-stream')
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True, use_reloader=False, host="0.0.0.0", port=8080)
